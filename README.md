3d70ad8b53fe157c7ac8a5f7d1ae4363d1fe3ce7
mjturt <maks.turtiainen@protonmail.com>
2018-01-15 03:46:17 +0200

    Valmis

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# asteriski-admin-theme
-Wordpress admin theme
+# asteriski-admin-theme plugin
+Wordpress-plugini joka vaihtaa admin-puolen ulkoasua
+Plugin tekee seuraavaa:
+1. Vaihtaa valikkojen väriä (vihreä/keltainen)
+2. Tekee tästä väriteemasta oletusteeman
+3. Vaihtaa vasemmassa yläkylmassa olevan Wordpress-logon Asteriskin logoksi
+4. Vaihtaa login-sivun Wordpress-logon Asteriskin logoksi
--- admin-color-schemes.php
+++ admin-color-schemes.php
@@ -7,11 +7,10 @@
  * Author: Maks Turtiainen
  * Author URI: http://asteriski.fi
  * License: GPLv2
- */
+ **/
 
 /*
-Original version published with GPLv2 by Kelly Dwan, Mel Choyce, Dave Whitley, Kate Whitley
-Changes by Maks Turtiainen 
+CSS/SCSS-files published with GPLv2 by Kelly Dwan, Mel Choyce, Dave Whitley, Kate Whitley
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -19,59 +18,61 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /* Korvataan kulmassa oleva Wordpressin logo Asteriskin logolla */
+
 function asteriski_custom_logo() {
-echo '
-<style type="text/css">
-#wpadminbar #wp-admin-bar-wp-logo > .ab-item .ab-icon:before { 
-background-image: url(' . get_bloginfo('stylesheet_directory') . '/admin-logo.png)  !important; 
-color: transparent;
+    echo '
+    <style type="text/css">
+        #wpadminbar #wp-admin-bar-wp-logo > .ab-item .ab-icon:before { 
+        background: url(' . plugins_url( 'img/admin-logo-asteriski.svg', __FILE__  ) . ') no-repeat scroll 0 0 / 100% auto !important; 
+        color: transparent;
+        }   
+        #wpadminbar #wp-admin-bar-wp-logo.hover > .ab-item .ab-icon {
+        background-position: 0 0;
+        }   
+    </style>
+    ';
 }
-#wpadminbar #wp-admin-bar-wp-logo.hover > .ab-item .ab-icon {
-background-position: 0 0;
-}   
- </style>
+
+add_action('wp_before_admin_bar_render', 'asteriski_custom_logo'); 
+
+/* Väriteeman lataus */
+
+$colors = array(
+    'asteriski',
+);
+
+function add_colors() {
+    wp_admin_css_color(
+        'asteriski', __( 'Asteriski', 'admin_schemes' ),
+        plugins_url( "asteriski/colors.css", __FILE__ ),
+        array( '#286e33', '#c19a12', '#d66621', '#348d42' ),
+        array( 'base' => '#286e33', 'focus' => '#c19a12', 'current' => '#d66621' )
+    );
+}
+add_action( 'admin_init', 'add_colors' );
+
+/* Asettaa Asteriskin admin teeman oletusteemaksi */
+
+function set_default_admin_color($user_id) {
+$args = array(
+            'ID' => $user_id,
+                    'admin_color' => 'asteriski'
+                        
+                );  
+    wp_update_user( $args  );
+}
+add_action('user_register', 'set_default_admin_color');
+
+/* Login-sivun logon */
+
+function asteriski_custom_login_logo() {
+echo '
+<style type=text/css>
+    body.login div#login h1 a {
+        background-image: url(' . plugins_url( 'img/login-asteriski.png', __FILE__  ) . ');
+        padding-bottom: 20px;
+    }
+</style>
 ';
 }
-
-
-/* Väriteema */
-class ACS_Color_Schemes {
-
-	/**
-	 * List of colors registered in this plugin.
-	 *
-	 * @since 1.0
-	 * @access private
-	 * @var array $colors List of colors registered in this plugin.
-	 *                    Needed for registering colors-fresh dependency.
-	 */
-	private $colors = array(
-		'asteriski',
-	);
-
-	function __construct() {
-		add_action( 'admin_init' , array( $this, 'add_colors' ) );
-        add_action('wp_before_admin_bar_render', 'asteriski_custom_logo');
-	}
-
-	/**
-	 * Register color scheme.
-	 */
-	function add_colors() {
-		$suffix = is_rtl() ? '-rtl' : '';
-
-		wp_admin_css_color(
-			'asteriski', __( 'Asteriski', 'admin_schemes' ),
-			plugins_url( "asteriski/colors$suffix.css", __FILE__ ),
-			array( '#286e33', '#c19a12', '#d66621', '#348d42' ),
-			array( 'base' => '#286e33', 'focus' => '#c19a12', 'current' => '#d66621' )
-		);
-	}
-
-
-}
-global $acs_colors;
-$acs_colors = new ACS_Color_Schemes;
-?>
-
-
+add_action( 'login_enqueue_scripts', 'asteriski_custom_login_logo' );
